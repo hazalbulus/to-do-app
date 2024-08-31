494838d1e4d198dd0f4f86c4a2fdde062f798c17
Hazal Buluş <hazal.bulus@ug.bilkent.edu.tr>
2024-08-31 18:48:09 +0300

    chore: set up new React app with Vite

---
+++ app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import Layout from './pages/Layout'
+
+
+function App() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
+
+
